49ba42e9a2151bcbb543e54ddb94bd741cfdcb2b
moacirrochadev <empreendedormoacirneto@gmail.com>
2023-09-27 23:37:51 -0300

    project finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "0",
+    "title": "títuloaqui",
+    "description": "textoaqui",
+    "photoCover":""
+  },
+  {
+    "id": "1",
+    "title": "títuloaqui",
+    "description": "textoaqui",
+    "photoCover":""
+  },
+]

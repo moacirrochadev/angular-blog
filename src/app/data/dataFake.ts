ffd68b7727da185e17d84be9aab364067fc3449f
moacirrochadev <empreendedormoacirneto@gmail.com>
2023-09-28 11:51:57 -0300

    blog of mir4 mmorpg finished

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id": "0",
-    "title": "títuloaqui",
-    "description": "textoaqui",
-    "photoCover":""
+    "title": "MERCADO UNIFICADO",
+    "description": "Os itens registrados antes da atualização do Mercado Unificado terão seu status alterado para\. Os itens registrados no Mercado existente devem ser recuperados para registrar o item no Mercado Unificado\. O Mercado Unificado não estará disponível nos servidores Expedição e Dominação\.",
+    "photoCover":"../../assets/imagens/banner_mir4.jpg"
   },
   {
     "id": "1",
     "title": "títuloaqui",
     "description": "textoaqui",
-    "photoCover":""
+    "photoCover":"../../assets/imagens/banner_mir4"
   },
 ]
